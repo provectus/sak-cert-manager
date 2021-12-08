2b7349834b926ceca0fd4bab6a365d4112e0e596
peteriosipov <piosipov@provectus.com>
2021-12-08 16:13:35 +0300

    linter config

---
+++ outputs.tf
@@ -0,0 +1,3 @@
+output "aws_vpc_arn" {
+  value = data.aws_vpc.main.arn
+}
--- variables.tf
+++ variables.tf
@@ -1,44 +1,53 @@
 variable "cluster_name" {
   type    = string
   default = ""
+  description = "Name of the cluster"
 }
 
 variable "domains" {
   type    = list(string)
   default = []
+  description = "domains"
 }
 
 variable "email" {
   type    = string
   default = ""
+  description = "email"
 }
 
 variable "environment" {
   type    = string
   default = ""
+  description = "environment"
 }
 
 variable "module_depends_on" {
   type    = list(any)
   default = []
+  description = "dependencies"
 }
 
 variable "namespace" {
   type    = string
   default = "cert-manager"
+  description = "namespace"
 }
 
 variable "project" {
   type    = string
   default = ""
+  description = "project"
 }
 
 variable "vpc_id" {
   type = string
+  description = "vpc id"
 }
 
 variable "zone_id" {
   type = string
+  description = "zone id"
 }
 
 variable "argocd" {
